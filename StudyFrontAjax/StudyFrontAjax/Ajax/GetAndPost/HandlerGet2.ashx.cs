b7da6e6d8b409191f1bed160e0c4bcd48c68f67e
张龙涛 <zlt389815117outlook.com>
2019-04-21 09:59:10 +0800

    --添加：get()，返回数据格式为xml

--- StudyFrontAjax/StudyFrontAjax/Ajax/GetAndPost/HandlerGet2.ashx.cs
+++ StudyFrontAjax/StudyFrontAjax/Ajax/GetAndPost/HandlerGet2.ashx.cs
@@ -13,8 +13,19 @@ namespace StudyFrontAjax.Ajax.GetAndPost
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
+            context.Response.ContentType = "application/xml";
+            string userName, content;
+            userName = context.Request.QueryString["userName"];
+            content = context.Request.QueryString["content"];
+
+            // 输出xml
+            context.Response.Write("<?xml version='1.0' encoding='utf-8' ?>");
+            context.Response.Write("<users>");
+            context.Response.Write("<user username='" + userName + "'>");
+            context.Response.Write("<cotent>" + content + "</cotent>");
+            context.Response.Write("</user>");
+            context.Response.Write("</users>");
+            context.Response.End();
         }
 
         public bool IsReusable
