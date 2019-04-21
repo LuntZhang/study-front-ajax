0b8b9316fd3cbb6eef18811845e05019b9e390f8
张龙涛 <zlt389815117outlook.com>
2019-04-21 10:33:22 +0800

    --添加：gei(),返回Json数据格式

---
+++ StudyFrontAjax/StudyFrontAjax/Ajax/GetAndPost/PingLun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudyFrontAjax.Ajax.GetAndPost
+{
+    public class PingLun
+    {
+        public string UserName { get; set; }
+        public string Content { get; set; }
+        public string Remark { get; set; }
+    }
+}
