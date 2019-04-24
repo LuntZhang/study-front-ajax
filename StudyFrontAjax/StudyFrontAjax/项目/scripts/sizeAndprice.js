d16a26c557363c09728cf61e7390eb879ae894ca
张龙涛 <zlt389815117outlook.com>
2019-04-24 22:42:54 +0800

    --学习项目视频

---
+++ StudyFrontAjax/StudyFrontAjax/项目/scripts/sizeAndprice.js
@@ -0,0 +1,18 @@
+﻿/*衣服尺寸选择*/
+$(function(){
+	$(".pro_size li").click(function(){
+		$(this).addClass("cur").siblings().removeClass("cur");
+		$(this).parent().siblings("strong").text(  $(this).text() );
+	})
+})
+/*数量和价格联动*/
+$(function () {
+    
+    var $span = $(".tbDetailPrice strong");
+	var price = $span.text();	
+	$("#num_sort").change(function(){
+	    var num = $(this).val();
+		var amount = num * price;
+		$(".pro_price strong").text(amount);
+	}).change();
+})
